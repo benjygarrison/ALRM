54175d2295723c4269b0a5827f08e387d2e26067
benjyGarrison <benjygarrison@gmail.com>
2019-08-28 01:55:31 -0700

    Added second view controller

---
+++ ALRM/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  ALRM
+//
+//  Created by Benjamin Garrison on 8/28/19.
+//  Copyright © 2019 Benji Garrison. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
