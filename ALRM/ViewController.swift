8556c0832b74215cf4e01dad103109bee0e59341
benjyGarrison <benjygarrison@gmail.com>
2019-08-28 00:36:26 -0700

    Initial Commit

---
+++ ALRM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ALRM
+//
+//  Created by Benjamin Garrison on 8/28/19.
+//  Copyright © 2019 Benji Garrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
